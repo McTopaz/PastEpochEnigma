9767513202db6b020215e0353954f05e26e285cf
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-11 15:37:06 +0100

    Added Init, Splash and Run methods and the main game loop.

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -4,15 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Terminal.Screens;
+
 namespace Terminal
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.Unicode;
-            new Screens.Splash().Run();
-            new Screens.MainMenu().Run();
+            Init();
+            //ShowSplash();
+            Run();
         }
+
+        private static void Init()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+        }
+
+        private static void ShowSplash()
+        {
+            var splash = new Splash();
+            splash.Show();
+        }
+
+        private static void Run()
+        {
+            var mainMenu = new MainMenu();
+
+            while(true)
+            {
+                mainMenu.Show();
+            }
+        }
+
     }
 }
