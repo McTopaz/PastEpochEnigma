c6839c1c25ec0735492e468722555c66577618b6
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-11 14:10:50 +0100

    Added constants for creating a box in the console.

---
+++ Terminal/BoxParts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal
+{
+    internal static class BoxParts
+    {
+        // Corners
+        public const string LeftUpperCorner = "\u256D";
+        public const string RightUpperCorner = "\u256E";
+        public const string LeftLowerCorner = "\u2570";
+        public const string RightLowerCorner = "\u256F";
+
+        // Lines
+        public const string HorizontalLine = "\u2500";
+        public const string VerticalLine = "\u2502";
+
+        // T-crossing
+        public const string LeftTCrossing = "\u251C";
+        public const string TopTCrossing = "\u252C";
+        public const string RightTCrossing = "\u2524";
+        public const string BottomTCrossing = "\u2534";
+
+        // Cross
+        public const string Cross = "\u253C";
+    }
+}
