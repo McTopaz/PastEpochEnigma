4d0e1393a447a79911c7dcc8b43dd5b5e6a79607
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-11 15:20:27 +0100

    Added folder for icons and Checkbox icons.

---
+++ Terminal/Icons/CheckboxIcons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal.Models
+{
+    internal class CheckBoxIcons
+    {
+        public const string EmptyBox = "\u2610";
+        public const string CheckedBox = "\u2611";
+    }
+}
