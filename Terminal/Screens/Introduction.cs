41b55728d388c837f8d8a8685ec19e1e82cde3e1
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-11 16:00:43 +0100

    WIP Introduction screen.

---
+++ Terminal/Screens/Introduction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal.Screens
+{
+    internal class Introduction : MenuBase
+    {
+        public override void Show()
+        {
+            Console.Clear();
+            base.DisplayHeader();
+            base.DisplayLogo(Resources.IntroductionLogo);
+            base.DisplayEmptyLine();
+            base.DisplayEmptyLine();
+
+            base.TakeInput();
+        }
+
+        protected override void ExecuteCommand(ConsoleKeyInfo input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
