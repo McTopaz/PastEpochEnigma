c4f92a8e04e62c4285aef07b7d67f4efc2af2ba2
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-12 07:42:59 +0100

    Added arrow icons.

---
+++ Terminal/Icons/ArrowsIcons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal.Icons
+{
+    internal class ArrowsIcons
+    {
+        public const string Up = "\u2191";
+        public const string Down = "\u2193";
+    }
+}
