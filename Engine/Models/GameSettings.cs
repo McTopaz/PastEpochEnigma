adda763878f80b2ff5d1575feb86f26e29dc41c6
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-12 07:57:18 +0100

    Added a GameSettings model.

---
+++ Engine/Models/GameSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class GameSettings
+    {
+        public DifficultLevel DifficultLevel { get; set; }
+        public GameMode GameMode { get; set; }
+    }
+}
