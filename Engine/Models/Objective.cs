c2536aa8baeb0003792dabf192d6ab7fdc7b2f23
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-12 08:48:05 +0100

    Added the objective Model.

---
+++ Engine/Models/Objective.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Objective
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsFinished { get; set; } = false;
+    }
+}
