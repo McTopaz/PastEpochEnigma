165eb64ac7e1c35fceb1cd408df6b5e3e0b69670
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-12 09:30:42 +0100

    Added an Awarness model.

---
+++ Engine/Models/Awarness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Awarness
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public void CalculateDuration()
+        {
+            Duration = End - Start;
+        }
+    }
+}
