087c26096c1193dcae68a4ee0d60b25f466a640d
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-12 13:11:25 +0100

    No changes.

---
+++ Engine/Models/Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Game
+    {
+        public GameSettings GameSettings { get; set; }
+        public MissionHandler MissionHandler { get; set; }
+
+        public Awarness Awarness { get; set; } = new Awarness();
+        public List<Mission> Missions { get; set; } = new List<Mission>();
+        public int CurrentMission { get; set; } = 0;
+
+
+        public Game(GameSettings gameSettings, MissionHandler missionHandler)
+        {
+            GameSettings = gameSettings;
+            MissionHandler = missionHandler;
+        }
+
+        public void Init()
+        {
+            var mission = MissionHandler.LoadMission1FromAsset();
+            Missions.Add(mission);
+        }
+
+        public void Start()
+        {
+
+        }
+    }
+}
