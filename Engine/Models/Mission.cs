5b17e71adee515239609a648ea158bd5084a226f
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-12 08:47:56 +0100

    Added the Mission model.

---
+++ Engine/Models/Mission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Mission
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+
+    }
+}
