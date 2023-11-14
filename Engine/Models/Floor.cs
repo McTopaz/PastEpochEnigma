f7556036efa863b9ce21d1a0b90c958b875cfb20
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-14 11:26:07 +0100

    Added Above and Below floor references.

--- Engine/Models/Floor.cs
+++ Engine/Models/Floor.cs
@@ -10,11 +10,13 @@ namespace Engine.Models
     {
         public string Name { get; set; }
         public bool IsStart { get; set; } = false;
+        public bool IsOptional { get; set; }
         public Awarness Awarness { get; set; } = new Awarness();
         public List<Room> Rooms { get; set; }
         public (int X, int Y) Position { get; set; }
-        public bool IsOptional { get; set; }
         public DifficultLevel DifficultLevel { get; set; } = DifficultLevel.Easy;
+        public Floor? Above { get; set; }
+        public Floor? Below { get; set; }
 
         public override string ToString() =>  IsOptional ? $"{ Name } - Optional: { DifficultLevel }" : Name;
     }
