f85b507d87acb7f8ea690b09c77a9e9b647abf3d
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-14 11:17:31 +0100

    Added the Romm model.

--- Engine/Models/Room.cs
+++ Engine/Models/Room.cs
@@ -8,5 +8,10 @@ namespace Engine.Models
 {
     public class Room
     {
+        public bool IsStart { get; set; }
+        public bool IsEnd { get; set; }
+        public bool IsDark { get; set; }
+        public bool HasLockedDoor { get; set; }
+        public bool HasItem { get; set; }
     }
 }
