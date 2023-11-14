d78fd0ef630c74f6d3bb5003e106568eced520a7
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-14 11:18:14 +0100

    Added the Floor model.

--- Engine/Models/Floor.cs
+++ Engine/Models/Floor.cs
@@ -8,6 +8,14 @@ namespace Engine.Models
 {
     public class Floor
     {
+        public string Name { get; set; }
+        public bool IsStart { get; set; } = false;
+        public Awarness Awarness { get; set; } = new Awarness();
+        public List<Room> Rooms { get; set; }
+        public (int X, int Y) Position { get; set; }
+        public bool IsOptional { get; set; }
+        public DifficultLevel DifficultLevel { get; set; } = DifficultLevel.Easy;
 
+        public override string ToString() =>  IsOptional ? $"{ Name } - Optional: { DifficultLevel }" : Name;
     }
 }
