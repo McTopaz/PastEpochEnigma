f34a514e8bdd12cb1a46220ef139059844c54f78
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-14 12:41:34 +0100

    Removed MissionLoader from Game model.

--- Engine/Models/Game.cs
+++ Engine/Models/Game.cs
@@ -9,17 +9,14 @@ namespace Engine.Models
     public class Game
     {
         public Settings Settings { get; set; }
-        public MissionLoader MissionHandler { get; set; }
-
         public Awarness Awarness { get; set; } = new Awarness();
         public List<Mission> Missions { get; set; } = new List<Mission>();
 
         private int _index = 0;
 
-        public Game(Settings settings, MissionLoader missionHandler)
+        public Game(Settings settings)
         {
             Settings = settings;
-            MissionHandler = missionHandler;
         }
 
         public Mission GetCurrentMission()
