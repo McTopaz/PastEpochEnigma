2036e0e2d07002a22f74c47879177e6a74148c3b
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-14 11:19:12 +0100

    Added custom ToString.

--- Engine/Models/Objective.cs
+++ Engine/Models/Objective.cs
@@ -11,5 +11,7 @@ namespace Engine.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public bool IsFinished { get; set; } = false;
+
+        public override string ToString() => Name;
     }
 }
