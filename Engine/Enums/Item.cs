aaf115099e9857d041a8f78326523047acca23de
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-14 21:53:09 +0100

    Added enum for game Items.

---
+++ Engine/Enums/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Enums
+{
+    public enum Item
+    {
+        Flashlight,
+        KeyCardOperator,
+        KeyCardManager,
+        KeyCardAdministrator
+    }
+}
