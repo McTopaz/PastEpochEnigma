aa1a12062d59c24f8681571e558847c1b904bb83
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-15 17:41:40 +0100

    Minor method name clarification.

--- Engine/FloorFactory.cs
+++ Engine/FloorFactory.cs
@@ -19,12 +19,12 @@ namespace Engine
         public void InitFloors(Mission mission, DifficultLevel gameDifficultLevel)
         {
             mission.CurrentFLoor = mission.Floors.First(f => f.IsStart);
-            mission.Floors = mission.Floors.Where(f => IncludeForGameDifficultLevel(f.DifficultLevel, gameDifficultLevel)).ToList();
+            mission.Floors = mission.Floors.Where(f => IncludeFloorsForGameDifficultLevel(f.DifficultLevel, gameDifficultLevel)).ToList();
             mission.Floors.Reverse<Floor>().Aggregate((f1, f2) => f1.Above = f2);
             mission.Floors.Aggregate((f1, f2) => f1.Below = f2);
         }
 
-        private bool IncludeForGameDifficultLevel(DifficultLevel floorDifficultLevel, DifficultLevel gameDifficultLevel)
+        private bool IncludeFloorsForGameDifficultLevel(DifficultLevel floorDifficultLevel, DifficultLevel gameDifficultLevel)
         {
             if (floorDifficultLevel == DifficultLevel.Easy) return true;
             else if (floorDifficultLevel == DifficultLevel.Medium && (gameDifficultLevel == DifficultLevel.Medium || gameDifficultLevel == DifficultLevel.Hard)) return true;
