4c6ec96a707a6aa9f015ba3e593371486032fdc9
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-16 18:37:55 +0100

    Added a RoomSensor test.

---
+++ Test/RoomSorterTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestClass]
+    internal class RoomSorterTest
+    {
+        [TestMethod]
+        public void Key_Lock__Key_Lock()
+        {
+
+        }
+
+        [TestMethod]
+        public void Lock_Key__Key_Lock()
+        {
+
+        }
+    }
+}
