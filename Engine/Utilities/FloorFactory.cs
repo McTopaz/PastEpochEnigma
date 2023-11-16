03f8f7ee7185b2069a153f6c08127afdc3b4de1f
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-16 18:28:12 +0100

    Created the Utilities folder and moved FloorFactory and MissionLoader.

---
+++ Engine/Utilities/FloorFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+using Engine.Enums;
+using Engine.Models;
+
+namespace Engine.Utilities
+{
+    public class FloorFactory
+    {
+        public void InitFloors(Mission mission, DifficultLevel gameDifficultLevel)
+        {
+            mission.CurrentFLoor = mission.Floors.First(f => f.IsStart);
+            mission.Floors = mission.Floors.Where(f => IncludeFloorsForGameDifficultLevel(f.DifficultLevel, gameDifficultLevel)).ToList();
+            mission.Floors.Reverse<Floor>().Aggregate((f1, f2) => f1.Above = f2);
+            mission.Floors.Aggregate((f1, f2) => f1.Below = f2);
+        }
+
+        private bool IncludeFloorsForGameDifficultLevel(DifficultLevel floorDifficultLevel, DifficultLevel gameDifficultLevel)
+        {
+            if (floorDifficultLevel == DifficultLevel.Easy) return true;
+            else if (floorDifficultLevel == DifficultLevel.Medium && (gameDifficultLevel == DifficultLevel.Medium || gameDifficultLevel == DifficultLevel.Hard)) return true;
+            else if (floorDifficultLevel == DifficultLevel.Hard && gameDifficultLevel == DifficultLevel.Hard) return true;
+            else return false;
+        }
+    }
+}
--- Engine/Utilities/MissionLoader.cs
+++ Engine/Utilities/MissionLoader.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Collections.ObjectModel;
 
-namespace Engine
+namespace Engine.Utilities
 {
     public class MissionLoader
     {
