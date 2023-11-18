6b67737ef68f36f67a32071604fa7c2f624be02e
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-18 10:50:54 +0100

    Added MainDirection property.

--- Engine/Models/Room.cs
+++ Engine/Models/Room.cs
@@ -25,6 +25,7 @@ namespace Engine.Models
         public Room? Next { get; set; }
         public (int X, int Y) Location { get; set; } = (0, 0);
         public List<Side> Directions { get; set; } = new List<Side>();
+        public Side MainDirection { get; set; }
 
         public Item? Item
         {
