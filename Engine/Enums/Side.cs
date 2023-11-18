6665dda5928147e6a31bc99a6c705d1c23b6aeea
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-18 10:10:49 +0100

    Created enum with sides for a room.

---
+++ Engine/Enums/Side.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Enums
+{
+    public enum Side
+    {
+        Left = 0,
+        Up = 1,
+        Right = 2,
+        Bottom = 3
+    }
+}
