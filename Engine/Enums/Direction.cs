448a0b5865894b0436a2a6b6bc65d1c773101241
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-21 20:45:49 +0100

    Added the Direction enum.

---
+++ Engine/Enums/Direction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Enums
+{
+    public enum Direction
+    {
+        None,
+        Left,
+        Up,
+        Right,
+        Down
+    }
+}
