7ce05fffd5dcc642d5d7f395e339167d19113715
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-11-23 02:45:31 +0100

    Added room icons.

---
+++ Terminal/Icons/RoomIcons.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal.Icons
+{
+    internal class RoomIcons
+    {
+        public const string Door = "\U0001F6AA";
+        public const string GoalFlag = "\U0001F3C1";
+        public const string Spy = "\U0001F574";
+        public const string Guard = "\U0001F482";
+        public const string Document = "\U0001F5C9";
+    }
+}
