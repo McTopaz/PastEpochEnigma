8299c00f55ecf7439fee23b964f54accb3c7c6a8
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2024-03-03 16:06:35 +0100

    Added.

---
+++ Engine/Models/MoveResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class MoveResult
+    {
+        public bool CanMove { get; internal set; } = false;
+        public string Reason { get; internal set; } = string.Empty;
+    }
+}
