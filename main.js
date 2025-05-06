6bfd409b58036e260b68c8212889d7cbc37c2d8d
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-05-06 11:49:34 +0200

    Removed code which have been moved to appFlow.js and initApp.js.

--- main.js
+++ main.js
@@ -1,48 +1,7 @@
-import { GameTitle, GameVersion } from "./entities/constants.js";
-
-const SHOW_SPLASH = true;
+import { initializeApp } from "./usecases/initApp.js";
+import { runAppFlow } from "./usecases/appFlow.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-  document.title = `${GameTitle} - V${GameVersion}`;
-  document.getElementById("footer").textContent = `Version: ${GameVersion}`;
-
-  if (SHOW_SPLASH) {
-    loadSplashScreen();
-  } else {
-    startGame();
-  }
+  initializeApp();
+  runAppFlow();
 });
-
-function loadSplashScreen() {
-  fetch("view/splash/splash.html")
-    .then(response => response.text())
-    .then(html => {
-      const container = document.createElement("div");
-      container.innerHTML = html;
-      document.body.prepend(container);
-    })
-    .catch(err => {
-      console.error("Kunde inte ladda splash-screen:", err);
-      startGame();
-    });
-}
-
-window.startGame = () => {
-  const splash = document.getElementById("splash-screen");
-  if (splash) splash.remove();
-
-  loadMainScreen();
-};
-
-function loadMainScreen() {
-  fetch("view/main/main.html")
-    .then(response => response.text())
-    .then(html => {
-      const mainContainer = document.createElement("div");
-      mainContainer.innerHTML = html;
-      document.body.appendChild(mainContainer);
-    })
-    .catch(err => {
-      console.error("Kunde inte ladda huvudvyn:", err);
-    });
-}
