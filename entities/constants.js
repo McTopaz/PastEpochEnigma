415cd1be3c8d077049c1683cb263971485a006b4
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-05-12 10:50:27 +0200

    Added CreatedBy constant.

--- entities/constants.js
+++ entities/constants.js
@@ -1,4 +1,5 @@
 export const GameVersion = "1.0.0";
 export const GameTitle = "Epoch Past Enigma";
 export const GameSubTitle = "Some SubTitle";
+export const Createdby = "Mc_Topaz";
 export const SplashDuration = 2000;
