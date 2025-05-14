c408276943f62b354541a0371d3f73581233229f
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-05-14 12:06:02 +0200

    Logging the base path during index.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "./usecases/initApp.js";
 import { runAppFlow } from "./usecases/appFlow.js";
+import { BASE_PATH } from "./entities/urlPaths.js"
+
+console.log("Base path:", BASE_PATH);
 
 window.addEventListener("DOMContentLoaded", () => {
   initializeApp();
