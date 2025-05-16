38dcacaf638a3e20e3c7605e5d3df642a1cacfd5
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-05-16 09:37:28 +0200

    Added Introduction to ViewPaths.

--- entities/urlPaths.js
+++ entities/urlPaths.js
@@ -9,6 +9,7 @@ export const ViewPaths = {
   base: new ViewParts("base", BASE_PATH),
   splash: new ViewParts("splash", BASE_PATH),
   main: new ViewParts("main", BASE_PATH),
+  introduction: new ViewParts("introduction", BASE_PATH),
 };
 
 export const AsciiPaths = {
