9bbf8cf1009bd111c586fadeb1abc8f8c1a2a51a
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-05-16 09:39:03 +0200

    Added call to initKeyboardListeners.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { initializeApp } from "./usecases/initApp.js";
 import { runAppFlow } from "./usecases/appFlow.js";
 import { BASE_PATH } from "./entities/urlPaths.js"
+import { initKeyboardListeners } from './view/listeners/keyboardListener.js';
 
 console.log("Base path:", BASE_PATH);
 
 window.addEventListener("DOMContentLoaded", () => {
   initializeApp();
+  initKeyboardListeners();
   runAppFlow();
 });
