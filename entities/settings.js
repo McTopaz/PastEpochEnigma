7e4edf13d0f6a00a81bcbad5d475ec8deac40034
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-06-25 16:45:22 +0200

    Added getters for the normal and speedrun modes.

--- entities/settings.js
+++ entities/settings.js
@@ -1,4 +1,11 @@
 export const settings = {
     difficulty: "easy",
-    mode: "normal"
+    mode: "normal",
+
+    get isNormalMode() {
+        return this.mode === "normal";
+    },
+    get isSpeedrunMode() {
+        return this.mode === "speedrun";
+    },
 }
